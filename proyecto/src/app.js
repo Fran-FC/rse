7b762cee25758c0a3cbd18b74a2969bc7d1151de
Fran-FC <frafolcm@gmail.com>
2021-11-30 18:02:56 +0100

    changes in project

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -1,23 +1,17 @@
 const scraper = require("./electricity_bill_scraping")
 const hours = require("../config/hour_range");
-const { next } = require("cheerio/lib/api/traversing");
 
-const first_check = ()=>{
-    var price_hour, next_check;
-    // first check if it is past 8 pm
+const first_check = async ()=>{
     let date = new Date();
-    // if it is before 20:15, check prices of today, else, check for tomorrow's
-    if (date.getHours() < 20 || (date.getHours() === 20 && date.getMinutes() < 15)) {
-        price_hour = await scraper.scrap(today=true);
-        next_check = (20*3600+15*60+60) - (date.getHours()*3600+date.getMinutes()*60+date.getSeconds());
-    }
-    else {
-        price_hour = await scraper.scrap();            
-        next_check = (24*3600) - (20*3600+15*60+60) - (date.getHours()*3600+date.getMinutes()*60+date.getSeconds());
-    }
+    let prices = await scraper.scrap();
+    let lowest_price = Math.min(...prices.slice(date.getHours()));
+    let lowest_hour = prices.indexOf(lowest_price) + 1;
+
+    return {"hour":lowest_hour, "price": lowest_price/1000}
 }
 
 module.exports = {
-    start: async ()=>{
+    start: async()=>{
+        let first_res = await first_check();
     }
 }
--- proyecto/src/electricity_bill_scraping.js
+++ proyecto/src/electricity_bill_scraping.js
@@ -1,15 +1,10 @@
 const request = require("request");
 const cheerio = require('cheerio');
 
-//const url = 'https://www.tarifadeluz.com/mañana.php';
-const base_url = "http://www.tarifadeluz.com"
-
+const url = "http://www.tarifadeluz.com"
 
 module.exports = {
-    scrap: (today=false) => {
-        var url = base_url;
-        if(!today) url += "/manana.php";
-
+    scrap: () => {
         var p = new Promise((resolve, reject)=>{
             request({
                 method: 'GET',
@@ -19,13 +14,12 @@ module.exports = {
                 const $ = cheerio.load(body);
                 let rows = $("tr").toArray(); // get array of rows, of the page
                 
-                let price_hour = {}; // initialize array of prices per hour
+                let price_hour = new Array(24); // initialize array of prices per hour
                 for (let i = 3; i < 24+3; i++){
                     // gets the value of the second column of each row starting by the 4th, which happens to 
                     // be the starting of the data's table.
                         // see http://www.tarifadeluz.com and inspect the table to understand it
-                    //price_hour.push(rows[i].children[3].children[0].children[0].children[0].data); 
-                    price_hour[i-3] = rows[i].children[3].children[0].children[0].children[0].data;
+                    price_hour[i-3] = parseInt(parseFloat(rows[i].children[3].children[0].children[0].children[0].data)*1000);
                 }
                 resolve(price_hour);
             });
