946873f5dc0d8a1a252a75b2e9c8402c44910895
Fran-FC <frafolcm@gmail.com>
2022-01-13 19:42:52 +0000

    email message improved

--- trabajo_final/src/utils/email_notifyer.py
+++ trabajo_final/src/utils/email_notifyer.py
@@ -1,10 +1,11 @@
-from os import confstr
-import smtplib, ssl, json
+import smtplib, ssl, json, os
 import time
 
 port = 465 # ssl port for smtp
 
-config_path = "/home/pi/coding/rse/proyecto_py/config/config.json"
+pwd = os.getcwd()
+config_path = pwd + "/config/config.json"
+print(config_path)
 
 sender_email = ""
 password = ""
@@ -21,17 +22,16 @@ with open(config_path) as fd:
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-def send_email(msg):
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, reciever_email, msg)
-
 def notify_cheapest_hour(price_min, hour_min):
     msg = "El precio mas bajo es {0}kW/h a las {1} horas".format(price_min/1000, hour_min)
+    msg = msg + "\nTabla de precios en: http://www.tarifadeluz.com"
     if hour_min == 0:
         hour_min = 24
     send_email(msg)
     minutes_remaining = hour_min*60-time.localtime().tm_hour*60-time.localtime().tm_min
 
     time.sleep(minutes_remaining*60-1)
-    send_email(msg)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, reciever_email, msg)
